9211f1b34c0787c7f74fa2914d783b2d7d9fbbfc
JOZETHCPM1178 <enmanuelalex490@gmail.com>
2025-08-14 15:50:00 -0600

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,7 @@ const data = {
         { name: "Ishiruka", img: "https://github.com/JOZETHCPM1178/jozeth-web/raw/main/assets/icon.png", url: "https://www.mediafire.com/file/o4zfia09jjve16v/Ishiruka_Ishiruka_v16.apk/file", date: "2025-08-14" }
     ],
     games: [
-        { name: "Juego Ejemplo", img: "https://via.placeholder.com/300x200", url: "https://example.com/game.apk", date: "2025-08-12" }
+        { name: "HACK RESIDENT EVIL4", img: "https://github.com/JOZETHCPM1178/jozeth-web/raw/main/assets/resident.png", url: "https://www.mediafire.com/file/sef6qxi7mhaezeh/CHEATS_RESIDENT_EVIL_4.7z/file", date: "2025-08-14" }
         { name: "Resident evil 4", img: "https://github.com/JOZETHCPM1178/jozeth-web/raw/main/assets/resident.png", url: "https://www.mediafire.com/file/yimnvpsek5d1nl2/Resident_Evil_4_-_Wii_Edition_%2528USA%2529.nkit.7z/file", date: "2025-08-14"}
     ],
     files: [
