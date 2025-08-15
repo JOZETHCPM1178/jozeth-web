5ef9b870f8d7aad695ec02df0df914c1d0e197c3
JOZETHCPM1178 <enmanuelalex490@gmail.com>
2025-08-14 23:56:06 -0600

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
 const data = {
     apps: [
-        { name: "Ishiruka", img: "https://github.com/JOZETHCPM1178/jozeth-web/raw/main/assets/icon.png", url: "https://www.mediafire.com/file/o4zfia09jjve16v/Ishiruka_Ishiruka_v16.apk/file", date: "2025-08-14" }
+        { name: "Ishiruka", img: "https://github.com/JOZETHCPM1178/jozeth-web/raw/main/assets/icon.png", url: "https://www.mediafire.com/file/o4zfia09jjve16v/Ishiruka_Ishiruka_v16.apk/file", date: "2025-08-14" },
+        { name: "MT Manager", img
     ],
     games: [
         { name: "Drift Max Pro", img: "https://github.com/JOZETHCPM1178/jozeth-web/raw/main/assets/drift.png", url: "https://www.mediafire.com/file/89t5unkkc89ql0a/drift-max-pro-mod_2.5.89-an1.com.apk/file", date: "2025-08-14" },
