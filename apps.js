1b880d08fc4408d3c2eca92d4cd8da0b8e5f60c6
JOZETHCPM1178 <enmanuelalex490@gmail.com>
2026-03-04 21:34:32 -0600

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,26 @@
+{
+  "apps": [
+    {
+      "id": 1,
+      "nombre": "Car Parking Multiplayer MOD",
+      "categoria": "juegos",
+      "etiqueta": "🎮 JUEGO MOD",
+      "descripcion": "Multijugador de estacionamiento con coches modificados, dinero infinito y eventos online.",
+      "imagen": "https://via.placeholder.com/300x180/1a1a1a/ff00ff?text=Car+Parking",
+      "tamaño": "1 GB",
+      "version": "4.8.2",
+      "url": "https://www.mediafire.com/file/et46cxcu69ehaok/cpm48213modt-Appsfab.net.apk/file"
+    },
+    {
+      "id": 2,
+      "nombre": "Resident Evil 4 Wii Edition",
+      "categoria": "juegos",
+      "etiqueta": "🎮 JUEGO MOD",
+      "descripcion": "Clásico de terror en versión Wii para Android con texturas mejoradas.",
+      "imagen": "https://via.placeholder.com/300x180/1a1a1a/00ffff?text=Resident+Evil+4",
+      "tamaño": "4.2 GB",
+      "version": "Wii NTSC",
+      "url": "https://www.mediafire.com/file/yimnvpsek5d1nl2/Resident_Evil_4_-_Wii_Edition_%2528USA%2529.nkit.7z/file"
+    }
+  ]
+}
