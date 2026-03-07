be2dd12aced1e9dd68232f62af5379a8a9e2b2d3
JOZETHCPM1178 <enmanuelalex490@gmail.com>
2026-03-06 18:59:00 -0600

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,30 @@
+const apps = [
+
+{
+nombre:"Spotify MOD",
+categoria:"APK MOD",
+imagen:"https://i.imgur.com/OdL0XPt.png",
+descripcion:"Spotify premium desbloqueado",
+descarga:"https://mediafire.com/ejemplo",
+playstore:"https://play.google.com/store/apps/details?id=com.spotify.music"
+},
+
+{
+nombre:"Free Fire Script",
+categoria:"SCRIPT",
+imagen:"https://i.imgur.com/9z3s7Yp.png",
+descripcion:"Script optimizado para Free Fire",
+descarga:"https://mediafire.com/script",
+playstore:"#"
+},
+
+{
+nombre:"Tutorial Root Android",
+categoria:"VIDEOS TUTORIALES",
+imagen:"https://i.imgur.com/k7KQF5T.png",
+descripcion:"Aprende a rootear tu Android",
+descarga:"https://youtube.com",
+playstore:"#"
+}
+
+];
